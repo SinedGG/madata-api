a0b14dac6a6b790ba423c0eaf84a96d0593cb800
Sined <denis2320032@gmail.com>
2022-12-11 20:21:51 +0200

    🍃 | Changed SQL query

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const db = mysql.createPool({
 
 app.post("/getAnswer", async (req, res) => {
   const question = req.query.question;
-  console.log(question);
   const [answer] = await db.query(
-    `SELECT answers.text FROM questions INNER join answers ON questions.id = answers.question_id WHERE questions.text = '${question}'`
+    `SELECT answers.text FROM questions INNER join answers ON questions.id = answers.question_id WHERE questions.text = ?`,
+    [question]
   );
   res.json({ answer: answer });
 });
@@ -33,7 +33,8 @@ app.post("/getAnswer", async (req, res) => {
 app.post("/getFullAnswer", async (req, res) => {
   const question = req.query.question;
   const [out] = await db.query(
-    `SELECT questions.text as questions, answers.text as answers FROM questions INNER join answers ON questions.id = answers.question_id WHERE questions.text = '${question}'`
+    `SELECT questions.text as questions, answers.text as answers FROM questions INNER join answers ON questions.id = answers.question_id WHERE questions.text LIKE concat('%', ?, '%')`,
+    [question]
   );
   res.json({ out });
 });
@@ -43,18 +44,25 @@ app.post("/pushAnswer", async (req, res) => {
   const answer = req.query.answer;
   if (question && answer) {
     await db
-      .query(`insert into questions(text) values("${question}")`)
-      .catch((err) => {});
+      .query(`insert into questions(text) values (?)`, [question])
+      .catch((err) => {
+        console.log(err);
+      });
+
     try {
       await db.query(
-        `insert into answers(text, question_id) values("${answer}", (select id from questions where text = "${question}"))`
+        `insert into answers(text, question_id) values (?, (select id from questions where text = (?)))`,
+        [answer, question]
       );
       res.status(201).send("OK");
     } catch (err) {
+      if (err.message.includes(`You have an error in your SQL syntax`))
+        console.log(err);
+
       if (err.message.includes(`Duplicate entry`)) {
-        console.log(`Duplicate ${question} - ${answer}`);
-        res.json({ message: "NOT OK" });
+        console.log(`Дублікат питання: ${question} - ${answer}`);
       }
+      res.json({ message: "NOT OK" });
     }
   }
 });
