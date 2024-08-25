0452ebfcfaa972c523ab307d09a2132c3f6998a7
den <51417134+SinedGG@users.noreply.github.com>
2024-08-25 16:38:59 +0300

    Change config

--- madata.config.js
+++ madata.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "madata-api",
-      cwd: "/home/ubuntu/pm/madata-api",
+      cwd: "/var/pm2/apps/madata-api",
       script: "index.js",
       log_date_format: "YYYY-MM-DD HH:mm Z",
 
